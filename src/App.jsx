9c104e60ba3b2e81e75e4c73b70479ae31bd24e1
Michał Salabura <micheu334@gmail.com>
2023-09-04 22:57:31 +0200

    Revert "feat: create context for cart"
    This reverts commit af415e7eca01daadecbd9e19c3e69e8abf2c2c1c.

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,12 @@
-import { createContext } from 'react'
 import { Navbar } from './components/navbar'
 import './App.scss'
-import { useState } from 'react'
-
-export const CartContext = createContext({
-  amount: 0,
-  addToCart: () => {},
-  removeFromCart: () => {},
-})
 
 function App() {
-  const [amount, setAmount] = useState(0);
-
-  const addToCart = () => {
-    setAmount(amount + 1);
-  }
-  const removeFromCart = () => {
-    if(amount != 0) {
-      setAmount(amount - 1);
-    }
-  }
 
   return (
-    <CartContext.Provider value={{amount, addToCart, removeFromCart}}>
+    <>
       <Navbar />
-    </CartContext.Provider>
+    </>
   )
 }
 
