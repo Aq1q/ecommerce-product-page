8a7fbc2f49232a71c71b0e0250bb91adf694b52a
Michał Salabura <micheu334@gmail.com>
2023-09-05 21:33:47 +0200

    feat: create initial style

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -7,7 +7,7 @@ import Thumbnail1 from '../assets/image-product-1-thumbnail.jpg';
 import Thumbnail2 from '../assets/image-product-2-thumbnail.jpg';
 import Thumbnail3 from '../assets/image-product-3-thumbnail.jpg';
 import Thumbnail4 from '../assets/image-product-4-thumbnail.jpg';
-import Cart from '../assets/icon-cart.svg';
+import Cart from '../assets/icon-cart-white.svg';
 import Plus from '../assets/icon-plus.svg';
 import Minus from '../assets/icon-minus.svg';
 import '../styles/main.scss';
@@ -54,24 +54,24 @@ export const Display = () => {
                 <p className="description-text">
                     These low-profile sneakers are your perfect casual wear companion. Feature a durable rubber outer sole, they&apos;ll withstand everything the weather can offer.
                 </p>
-                <div>
-                    <div>
+                <div className="price-box">
+                    <div className="discount-box">
                         <h2 className="price">$125.00</h2>
                         <p className="discount">50%</p>
                     </div>
                     <p className="old-price">$250</p>
                 </div>
-                <div>
-                    <div>
-                        <div onClick={handlePlus}>
-                            <img src={Plus} alt="Plus" />
+                <div className="add">
+                    <div className="select">
+                        <div className="minus" onClick={handleMinus}>
+                            <img className="minus-img" src={Minus} alt="Minus" />
                         </div>
                         <p>{amountToAdd}</p>
-                        <div onClick={handleMinus}>
-                            <img src={Minus} alt="Minus" />
+                        <div className="plus" onClick={handlePlus}>
+                            <img className="plus-img" src={Plus} alt="Plus" />
                         </div>
                     </div>
-                    <button>
+                    <button className="add-button">
                         <img src={Cart} alt="" />
                         Add to cart
                     </button>
