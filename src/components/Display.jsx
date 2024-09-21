d654913dd35c0c98c3278affed8665e25fe8cf3b
Michał Salabura <micheu334@gmail.com>
2024-09-21 19:38:21 +0100

    Feat: create lightbox

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import Image1 from '../assets/image-product-1.jpg';
 import Image2 from '../assets/image-product-2.jpg';
 import Image3 from '../assets/image-product-3.jpg';
@@ -11,16 +11,25 @@ import Cart from '../assets/icon-cart-white.svg';
 import Plus from '../assets/icon-plus.svg';
 import Minus from '../assets/icon-minus.svg';
 import Close from '../assets/icon-close.svg';
+import Next from '../assets/icon-Next.svg';
+import Previous from '../assets/icon-Previous.svg';
 import '../styles/main.scss';
 import { CartContext } from "../App";
 
 export const Display = () => {
     const [mainImage, setMainImage] = useState(Image1);
+    const [lightboxImage, setLightboxImage] = useState(Image1);
     const [amountToAdd, setAmountToAdd] = useState(0);
+    const [imgNumber, setImgNumber] = useState(0);
+    const [showLightbox, setShowLightbox] = useState(false);
     const { addToCart } = useContext(CartContext);
 
     const thumbnailClick = (image) => {
         setMainImage(image)
+    }
+
+    const lbThumbnailClick = (image) => {
+        setLightboxImage(image)
     }
 
     const handlePlus = () => {
@@ -33,11 +42,34 @@ export const Display = () => {
         addToCart(amountToAdd);
     }
 
+    useEffect(() => {
+        // Accessing scss variable "lightbox-display"
+        // and changing the same according to the state of "darkTheme"
+        const root = document.documentElement;
+        root?.style.setProperty(
+            "--lightbox-display",
+            showLightbox ? "flex" : "none"
+        );
+    }, [showLightbox]);
+   
+
+    const changeThumbnail = (imgValue) => {
+        let images = [Image1, Image2, Image3, Image4];
+        if (imgNumber + imgValue < 0) {
+            setImgNumber(3);
+        } else if (imgNumber + imgValue > 3) {
+            setImgNumber(0);
+        } else {
+            setImgNumber(imgNumber + imgValue)
+        }
+        setLightboxImage(images[imgNumber])
+    }
+
     return (
         <div className="main" aria-label="main">
             <div className="photos">
                 <div className="image-box">
-                    <img className="main-image" src={mainImage} alt="" />
+                    <img className="main-image" onClick={() => {setShowLightbox(!showLightbox)}} src={mainImage} alt="" />
                 </div>
                 <div className="thumbnail-box">
                     <div className="thumbnail">
@@ -85,17 +117,34 @@ export const Display = () => {
             </div>
             <div className="lightbox">
                 <div className="lightbox-container">
-                    <div className="close-container">
-                        <img className="close" src={Close} alt="" />
+                    <div className="close-container" onClick={() => {setShowLightbox(!showLightbox)}}>
+                        <img className="close" onClick={() => {setShowLightbox(!showLightbox)}} src={Close} alt="" />
                     </div>
                     <div className="main-lightbox-img">
-                        <img className="lightbox-img" src={mainImage} alt="" />
+                        <img className="lightbox-img" src={lightboxImage} alt="" />
                     </div>
                     <div className="lightbox-thumbnails">
-
+                        <div className="thumbnail">
+                            <img className="thumb-img" onClick={() => {lbThumbnailClick(Image1)}} src={Thumbnail1} alt="thumbnail 1" />
+                        </div>
+                        <div className="thumbnail">
+                            <img className="thumb-img" onClick={() => {lbThumbnailClick(Image2)}} src={Thumbnail2} alt="thumbnail 2" />
+                        </div>
+                        <div className="thumbnail">
+                            <img className="thumb-img" onClick={() => {lbThumbnailClick(Image3)}} src={Thumbnail3} alt="thumbnail 3" />
+                        </div>
+                        <div className="thumbnail">
+                            <img className="thumb-img" onClick={() => {lbThumbnailClick(Image4)}} src={Thumbnail4} alt="thumbnail 4" />
+                        </div>
                     </div>
-                    <div className="left"></div>
-                    <div className="right"></div>
+                </div>
+                <div className="switch-buttons-container">
+                    <div className="change-btn chg-left">
+                        <img src={Previous} className="" alt="" onClick={() => {changeThumbnail(-1)}} />
+                    </div>
+                    <div className="change-btn chg-right">
+                        <img src={Next} alt="" className="" onClick={() => {changeThumbnail(1)}} />
+                    </div>
                 </div>
             </div>
         </div>
